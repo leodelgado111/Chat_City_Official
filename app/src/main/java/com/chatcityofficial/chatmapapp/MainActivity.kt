b84340639fcd620f193b632103c776d8e67bb931
leodelgado111 <leodelgado111@gmail.com>
2025-08-11 04:38:10 -0700

    Version 1.2
    Removed top bar. Made status bar transparent for more immersion.

--- app/src/main/java/com/chatcityofficial/chatmapapp/MainActivity.kt
+++ app/src/main/java/com/chatcityofficial/chatmapapp/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.chatcityofficial.chatmapapp
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -12,21 +12,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        // Make status bar transparent
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        
         setContentView(R.layout.activity_main)
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_saved,
-                R.id.navigation_home,
-                R.id.navigation_chats,
-                R.id.navigation_profile
-            )
-        )
-        
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
